1bc0ba031603fcfd2539923fad9a10756eb2e5e0
Susan McKinlay <mc.ellen93@gmail.com>
2024-03-25 17:03:10 +0100

    The code to calculate the one-way distance seems to work. Now it's just the interpolation of latitude - longitude that it is missing

--- os/one_way_dist.py
+++ os/one_way_dist.py
@@ -34,7 +34,18 @@ import math
 from geopy.distance import Point
 from geopy.distance import geodesic
 
+def calculate_distance(cA, cB):
+    """
+    :param cA : pair of point A (long, lat)
+    :param cB : pair of point B (long, lat)
+    :return distance in Km
+    """
+    return geopy.distance.geodesic(cA, cB).km
+
 def calc_dist_each_rand_loc(data, season):
+    """
+    Function to calculate total migration distance for each individual
+    """
     tot_dist_migr = data[(data[f'compl_{season}_track'] == 1) & (data['season'] == season) & (data['Juv'] == 1)].groupby('ID')['distance'].sum().reset_index()
     #& (data[f'adults_{season}_compl_migr'] == 1)
     tot_dist_migr['dist_rand_loc'] = tot_dist_migr['distance'].div(20).round()
@@ -42,14 +53,110 @@ def calc_dist_each_rand_loc(data, season):
     print(tot_dist_migr)
     return tot_dist_migr
 
+def prepare_dataset(data, season, indiv):
+    """
+    Function to drop duplicates of 'modelat' and 'modelon' and create a list of tuples with coordinates
+    """
+    data_indiv = data.loc[(data[f'compl_{season}_track'] == 1) & (data['season'] == season)\
+         & (data['Juv'] == 1) & (data['ID'] == indiv)]
+    # Remove modelat and modelon duplicates
+    data_indiv = data_indiv.drop_duplicates(subset=['modelat', 'modelon'], keep='last')
+    return data_indiv
+
+def from_df_to_list_of_tuples(data_indiv):
+    """
+    Function that takes two columns of a dataframe and returns a list of tuples
+    """
+    # Create a list of tuples of coordinates (lat, long)
+    path = list(zip(data_indiv.modelat, data_indiv.modelon))
+    return path
+
+def select_shortest_distance(points_indiv_1, points_indiv_2):
+    """
+    It gets a point 1 in Track A (list of tuples of coords)
+    and finds the shortest distance between point 1 and all the other
+    random points in Track B (list of tuples of coords) and appends it to a list called min_distance_list 
+    and so on until all the points are done. It returns a list with all the minimum distances
+    """
+    # Reverse the order of the tuples because right now it is (lat, long) but I need it like this
+    # (long, lat)
+    points_indiv_1 = [tuple(reversed(t)) for t in points_indiv_1]
+    points_indiv_2 = [tuple(reversed(t)) for t in points_indiv_2]
+    tot_distance_list = []
+    min_distance_list = []
+    i = 0
+    j = 0
+    for point_track_a in points_indiv_1:
+        for point_track_b in points_indiv_2:
+            #print('this is point Track A', point_track_a, 'this is point Track B', point_track_b)
+            distance = calculate_distance(point_track_a, point_track_b)
+            #print('calculated distance', distance, i)
+            tot_distance_list.append(distance)
+            i += 1
+        min_distance_list.append(min(tot_distance_list))
+        # reset tot_distance to an empty list so it resets every time it finishes the inner loop, so it only
+        # appends the distances of one point with all the other 20 points, it empties when it gets to point 2
+        # of Track A
+        tot_distance_list = []
+        #print('min distance list', min_distance_list, j)
+        j += 1
+    print(min_distance_list)
+    return min_distance_list
+
+def interpolate_lat_and_long(data_indiv, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17):
+    """
+    It finds the corresponding progressive number (value of the column 'prog_number') of the closest latitude
+    to the limits of 45 N and 17 N.
+    It then modifies that row with the interpolated latitude and longitude (lat = 45 or = 17).
+    It returns the dataframe of the individual with all the latitudes below 45 N and above 17 N.
+    """
+    # Interpolate location above latitude of 45 N and below latitude of 17 N
+    # Modify the coordinates closest to 17 N and 45 N to the interpolation I got from QGIS
+    # Get rid of locations above latitude 45 N and below latitude 17 N, so only select
+    # locations below 45 N and above 17 N
+    first_row_45 = data_indiv.loc[(data_indiv['modelat'] > 45)]
+    first_row_17 =  data_indiv.loc[(data_indiv['modelat'] < 17)]
+    #print('First coordinates that are above 45 N \n', first_row_45[['date_time','ID','modelat']])
+    #print('First coordinates that are below latitude 17 N \n', first_row_17[['date_time','ID','modelat']])
+    limit = 45
+    data_indiv_fixed = find_closest_values(first_row_45, limit, data_indiv, interpolated_lat_45, interpolated_long_45)
+    limit = 17
+    data_indiv_fixed = find_closest_values(first_row_17, limit, data_indiv, interpolated_lat_17, interpolated_long_17)
+    # Filter out the points above 45 N and below 17 N that we don't need, since we are not interested in the
+    # breeding and wintering areas, we only want to consider the migration track
+    data_indiv_fixed = data_indiv_fixed.loc[(data_indiv['modelat'] <= 45) & (data_indiv['modelat'] >= 17)]
+    print('data indiv fixed', data_indiv_fixed[['prog_number','ID','modelat']])
+    return data_indiv_fixed
+
+def find_closest_values(first_row, limit, data_indiv, interpolated_lat, interpolated_long):
+    """
+    It finds the closest value fo the column 'modelat' to the limits 45 N and 17 N.
+    It then gets the index of the closest value to change the value of 'modelat' and 'modelon'
+    to the interpolated latitude and longitude.
+    """
+    # Find values that are closest to given values and return the row index
+    difference = np.abs(limit - first_row['modelat'])
+    index_closest_value = np.argmin(difference, axis=0)
+    prog_number = first_row['prog_number'].iloc[index_closest_value]
+    data_indiv.loc[data_indiv['prog_number'] == prog_number, 'modelat'] = interpolated_lat
+    data_indiv.loc[data_indiv['prog_number'] == prog_number, 'modelon'] = interpolated_long
+    return data_indiv
+        
 def place_equally_distanced_points(path, n_samples):
+    """
+    It takes the list of tuples of coordinates (lat, long) of an individual and creates 20 equally distanced 
+    points on the track. It returns the coordinates of these points as a list of tuples (lat, lon).
+    """
     # Distance between each path coord.
     geod = Geodesic.WGS84
-    path_distance = [0]
+    path_distance = [0] # the distance in mbetween each coordinate (the original ones)
     for (start_lat, start_lon), (end_lat, end_lon) in zip(path[:-1], path[1:]):
         path_distance.append(geod.Inverse(start_lat, start_lon, end_lat, end_lon)['s12'])
+    # Cumulative sum of each distance segment
     path_distance_cum = np.cumsum(path_distance)
+    # Distance in m between each equally distanced point
     point_distance = np.linspace(0, path_distance_cum[-1], n_samples)
+    #print('pah distance cum', path_distance_cum, 'point distance', point_distance, 'path distance', path_distance)
 
     points = []
     for pd in point_distance:
@@ -72,11 +179,23 @@ def place_equally_distanced_points(path, n_samples):
     
         # Location along segment.
         line = geod.InverseLine(start_lat, start_lon, end_lat, end_lon)
-        print(line)
         g_point = line.Position(pd_between, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
         points.append((g_point['lat2'], g_point['lon2']))
     return points
 
+def plot_maps_migration(points, path):
+    """
+    This function converts the list of tuples into a dataframe with 'latitude' and 'longitude' as columns.
+    It then maps out the migration track of the individual with the equally distanced points. 
+    It returns the dataframe with the coordinates of the equally distanced points.
+    """
+    points_df = pd.DataFrame.from_records(points, columns =['latitude','longitude'])
+    data = pd.DataFrame.from_records(path, columns =['latitude','longitude'])
+    plt.plot(data['longitude'], data['latitude'],'b') # Draw blue line
+    plt.plot(data['longitude'], data['latitude'],'bs') 
+    plt.plot(points_df['longitude'], points_df['latitude'], 'rs') # Draw red squares
+    mplleaflet.show()
+    return points_df
 
 def main():
     if len(sys.argv) < 1:
@@ -86,23 +205,56 @@ def main():
     data = pd.read_csv(sys.argv[1])
     data['date_time'] = pd.to_datetime(data['date_time'])
     season = 'spr'
-    tot_dist_migr = calc_dist_each_rand_loc(data, season)
-    data_3cx = data.loc[(data[f'compl_{season}_track'] == 1) & (data['season'] == season)\
-         & (data['Juv'] == 1) & (data['ID'] == '5IK')] #& (data['distance'] != 0)]
-    # Remove modelat and modelon duplicates
-    data_3cx = data_3cx.drop_duplicates(subset=['modelat', 'modelon'], keep='last')
-    path = list(zip(data_3cx.modelat, data_3cx.modelon))
-    n_samples = 18
-    points = place_equally_distanced_points(path, n_samples)
-    points_df = pd.DataFrame.from_records(points, columns =['latitude','longitude'])
-    print(path, type(path))
-    data = pd.DataFrame.from_records(path, columns =['latitude','longitude'])
-    plt.plot(data['longitude'], data['latitude'],'b') # Draw blue line
-    plt.plot(data['longitude'], data['latitude'],'bs') 
-    plt.plot(points_df['longitude'], points_df['latitude'], 'rs') # Draw red squares
-    mplleaflet.show()
-    #df_loc.to_csv(sys.argv[3], index = False)
+    # Only consider juveniles with complete tracks 
+    data = data[(data[f'compl_{season}_track'] == 1) & (data['season'] == season) & (data['Juv'] == 1)]
+    #tot_dist_migr = calc_dist_each_rand_loc(data, season)
+    n_samples = 22
+    # split each bird into a different dataframe 
+    #d = {}
+    #for name, group in data.groupby(['ID']):
+        #d['group_' + str(name)] = group  # group is a dataframe containing information about ONLY ONE BIRD
+        #group, path = prepare_dataset(data, season)
+        #points = place_equally_distanced_points(path, n_samples)
+        #plot_maps_migration(points, path)
+    # For individual 5IK
+    indiv = '5IK'
+    data_indiv_5IK = prepare_dataset(data, season, indiv)
+    # Interpolated latitude and longitude for the individual 5IK
+    interpolated_lat_17 = 17
+    interpolated_long_17 = 13.74
+    interpolated_lat_45 = 45
+    interpolated_long_45 = 16.63
+    data_indiv_5IK = interpolate_lat_and_long(data_indiv_5IK, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17)
+    path_5IK = from_df_to_list_of_tuples(data_indiv_5IK)
+    points_5IK = place_equally_distanced_points(path_5IK, n_samples)
+    points_df_5IK = plot_maps_migration(points_5IK, path_5IK)
+    points_df_5IK.to_csv(sys.argv[2], index = False)
+    # For individual 5LK
+    indiv = '5LK'
+    data_indiv_5LK = prepare_dataset(data, season, indiv)
+    interpolated_lat_17 = 17
+    interpolated_long_17 = 10.141
+    interpolated_lat_45 = 45
+    interpolated_long_45 = 9.69
+    data_indiv_5LK = interpolate_lat_and_long(data_indiv_5LK, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17)
+    path_5LK = from_df_to_list_of_tuples(data_indiv_5LK)
+    points_5LK = place_equally_distanced_points(path_5LK, n_samples)
+    points_df_5LK = plot_maps_migration(points_5LK, path_5LK)
+    points_df_5LK.to_csv(sys.argv[3], index = False)
 
+    # Calculate one-way distance for individual 5IK
+    min_distance_list_5IK = select_shortest_distance(points_5IK, points_5LK)
+    sum_distances_5IK = sum(min_distance_list_5IK)
+    tot_dist_migr = calc_dist_each_rand_loc(data, season)
+    sum_5IK = sum_distances_5IK/tot_dist_migr.loc[tot_dist_migr['ID'] == '5LK', 'distance'].values[0]
+
+    # Calculate one-way distance for individual 5LK
+    min_distance_list_5LK = select_shortest_distance(points_5LK, points_5IK)
+    sum_distances_5LK = sum(min_distance_list_5LK)
+    sum_5LK = sum_distances_5LK/tot_dist_migr.loc[tot_dist_migr['ID'] == '5IK', 'distance'].values[0]
+    print(len(min_distance_list_5IK), len(min_distance_list_5LK))
+    # Final one-way distance
+    print('final one-way distance in km', sum_distances_5IK, sum_distances_5LK, sum_5IK, sum_5LK, (sum_5IK + sum_5LK)/2)
 
 
 
