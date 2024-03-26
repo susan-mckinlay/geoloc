d71c8e8b2a72352918df3f60ebb7c4dd1b0d3fee
Susan McKinlay <mc.ellen93@gmail.com>
2024-03-26 16:55:47 +0100

    I corrected the total distance of the migration track, recalculating the distances of each segment after cutting off the track at 45 and 17 of latitude

--- os/one_way_dist.py
+++ os/one_way_dist.py
@@ -42,6 +42,28 @@ def calculate_distance(cA, cB):
     """
     return geopy.distance.geodesic(cA, cB).km
 
+def add_distance_in_dataframe(group):
+    """
+    add distance field in the dataframe.
+    returns a dataframe with the distance field
+    """
+    distance = 0
+    array_dist = []
+    i = 0
+    for _, row in group.iterrows():
+        if i == 0:
+            # first instance
+            prev_c = (row['modelat'], row['modelon'])
+            current_c = prev_c
+        else:
+            current_c = (row['modelat'], row['modelon'])
+        distance = calculate_distance(prev_c, current_c)
+        array_dist.append(distance)
+        prev_c = current_c
+        i += 1
+    group['distance'] = array_dist
+    return group
+
 def calc_dist_each_rand_loc(data, season):
     """
     Function to calculate total migration distance for each individual
@@ -55,7 +77,7 @@ def calc_dist_each_rand_loc(data, season):
 
 def prepare_dataset(data, season, indiv):
     """
-    Function to drop duplicates of 'modelat' and 'modelon' and create a list of tuples with coordinates
+    Function to drop duplicates of 'modelat' and 'modelon' 
     """
     data_indiv = data.loc[(data[f'compl_{season}_track'] == 1) & (data['season'] == season)\
          & (data['Juv'] == 1) & (data['ID'] == indiv)]
@@ -88,22 +110,19 @@ def select_shortest_distance(points_indiv_1, points_indiv_2):
     j = 0
     for point_track_a in points_indiv_1:
         for point_track_b in points_indiv_2:
-            #print('this is point Track A', point_track_a, 'this is point Track B', point_track_b)
             distance = calculate_distance(point_track_a, point_track_b)
-            #print('calculated distance', distance, i)
             tot_distance_list.append(distance)
             i += 1
         min_distance_list.append(min(tot_distance_list))
         # reset tot_distance to an empty list so it resets every time it finishes the inner loop, so it only
-        # appends the distances of one point with all the other 20 points, it empties when it gets to point 2
+        # appends the distances of one point with all the other 20 points, the list empties when it gets to point 2
         # of Track A
         tot_distance_list = []
-        #print('min distance list', min_distance_list, j)
         j += 1
     print(min_distance_list)
     return min_distance_list
 
-def interpolate_lat_and_long(data_indiv, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17):
+def change_lat_and_long(data_indiv, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17):
     """
     It finds the corresponding progressive number (value of the column 'prog_number') of the closest latitude
     to the limits of 45 N and 17 N.
@@ -134,9 +153,7 @@ def find_closest_values(first_row, limit, data_indiv, interpolated_lat, interpol
     It then gets the index of the closest value to change the value of 'modelat' and 'modelon'
     to the interpolated latitude and longitude.
     """
-    # Find values that are closest to given values and return the row index
-    difference = np.abs(limit - first_row['modelat'])
-    index_closest_value = np.argmin(difference, axis=0)
+    index_closest_value = find_index_closest_value(first_row, limit)
     prog_number = first_row['prog_number'].iloc[index_closest_value]
     data_indiv.loc[data_indiv['prog_number'] == prog_number, 'modelat'] = interpolated_lat
     data_indiv.loc[data_indiv['prog_number'] == prog_number, 'modelon'] = interpolated_long
@@ -149,35 +166,51 @@ def place_equally_distanced_points(path, n_samples):
     """
     # Distance between each path coord.
     geod = Geodesic.WGS84
-    path_distance = [0] # the distance in mbetween each coordinate (the original ones)
+    path_distance = [0] # the distance in between each coordinate (the original ones)
     for (start_lat, start_lon), (end_lat, end_lon) in zip(path[:-1], path[1:]):
         path_distance.append(geod.Inverse(start_lat, start_lon, end_lat, end_lon)['s12'])
-    # Cumulative sum of each distance segment
+    # Cumulative sum of each distance segment (of the original coordinates)
     path_distance_cum = np.cumsum(path_distance)
+    # np.linspace returns evenly spaced numbers over a specified interval, which in this case goes from 0
+    # to the last element of the cumulative sum
     # Distance in m between each equally distanced point
+    # The distance of each sampled point along the line will be between 0 and the total distance between all path coordinates.
     point_distance = np.linspace(0, path_distance_cum[-1], n_samples)
-    #print('pah distance cum', path_distance_cum, 'point distance', point_distance, 'path distance', path_distance)
 
     points = []
     for pd in point_distance:
-    
         # Find segment with.
-        i_start = np.argwhere(pd >= path_distance_cum)[:, 0][-1]
-    
+        # np.argwhere() returns the indices of elements that are non-zero.
+        # In this case it outputs indices of the distance between equally distanced points that is greater than
+        # the cumulative sum of the distances of the original coordinates (usually the next following point
+        # since we are using an array with cumulative sums)
+        # to first find which path segment each point lies on
+        i_start = np.argwhere(pd >= path_distance_cum)[:,0][-1]
+        # It only returns indices when this condition is met; i_start is the index of when the cum_distance of the equally
+        # distanced point is bigger than the array of cumulative distances of the original coordinates
         # Early exit for ends.
         if np.isclose(pd, path_distance_cum[i_start]):
-            points.append(path[i_start])
+        # When comparing arrays, Numpy’s isclose function returns an array of Boolean values, 
+        # with each element indicating whether the corresponding element in pd (cumulative dist. between eq.
+        # points) is close to the index cumulative sum of the distances between the original coordinates
+            points.append(path[i_start]) # The original coordinates indexed at the segment with the point
+            # So if the equally distanced point is very close to an end of a segment just append the coords
+            # of that point
             continue
         elif np.isclose(pd, path_distance_cum[-1]):
+            # So if the equally distanced point is very close to the last point of the path just append the coords
+            # of that point
             points.append(path[-1])
             continue
-        
         # Distance along segment.
+        # Get the coordinates of Point A and Point B (original coordinates) of the segment
         start_lat, start_lon = path[i_start]
         end_lat, end_lon = path[i_start + 1]
+        # pd_between is the distance along the segment at which I need to place the equally distanced point
+        # pd = cumulative sum of equally distanced point, i_start is the index at which 
         pd_between = pd - path_distance_cum[i_start]
-    
         # Location along segment.
+        # Construct a geodescic line instead of a straight segement between the original coordinates
         line = geod.InverseLine(start_lat, start_lon, end_lat, end_lon)
         g_point = line.Position(pd_between, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
         points.append((g_point['lat2'], g_point['lon2']))
@@ -195,7 +228,36 @@ def plot_maps_migration(points, path):
     plt.plot(data['longitude'], data['latitude'],'bs') 
     plt.plot(points_df['longitude'], points_df['latitude'], 'rs') # Draw red squares
     mplleaflet.show()
+    
+    distance = 0
+    array_dist = []
+    i = 0
+    for _, row in points_df.iterrows():
+        if i == 0:
+            # first instance
+            prev_c = (row['latitude'], row['longitude'])
+            current_c = prev_c
+        else:
+            current_c = (row['latitude'], row['longitude'])
+        distance = calculate_distance(prev_c, current_c)
+        array_dist.append(distance)
+        prev_c = current_c
+        i += 1
+    points_df['distance'] = array_dist
+    print(points_df)
+    print(points_df['distance'].sum())
+    
     return points_df
+
+def find_index_closest_value(data, limit):
+    # Find values that are closest to given values and return the row index
+    difference = np.abs(limit - data['modelat'])
+    index_closest_value = np.argmin(difference, axis=0)
+    return index_closest_value
+
+def interpolate_coords(data_indiv, limit):
+    print('hello')
+
 
 def main():
     if len(sys.argv) < 1:
@@ -204,57 +266,63 @@ def main():
     warnings.filterwarnings("ignore")
     data = pd.read_csv(sys.argv[1])
     data['date_time'] = pd.to_datetime(data['date_time'])
+    # Season
     season = 'spr'
     # Only consider juveniles with complete tracks 
     data = data[(data[f'compl_{season}_track'] == 1) & (data['season'] == season) & (data['Juv'] == 1)]
-    #tot_dist_migr = calc_dist_each_rand_loc(data, season)
+    # Number of equally distanced points
     n_samples = 22
-    # split each bird into a different dataframe 
-    #d = {}
-    #for name, group in data.groupby(['ID']):
-        #d['group_' + str(name)] = group  # group is a dataframe containing information about ONLY ONE BIRD
-        #group, path = prepare_dataset(data, season)
-        #points = place_equally_distanced_points(path, n_samples)
-        #plot_maps_migration(points, path)
     # For individual 5IK
     indiv = '5IK'
     data_indiv_5IK = prepare_dataset(data, season, indiv)
     # Interpolated latitude and longitude for the individual 5IK
     interpolated_lat_17 = 17
-    interpolated_long_17 = 13.74
+    interpolated_long_17 = 13.766689 #13.74
     interpolated_lat_45 = 45
-    interpolated_long_45 = 16.63
-    data_indiv_5IK = interpolate_lat_and_long(data_indiv_5IK, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17)
+    interpolated_long_45 = 16.617252 #16.63
+    data_indiv_5IK = change_lat_and_long(data_indiv_5IK, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17)
     path_5IK = from_df_to_list_of_tuples(data_indiv_5IK)
     points_5IK = place_equally_distanced_points(path_5IK, n_samples)
     points_df_5IK = plot_maps_migration(points_5IK, path_5IK)
-    points_df_5IK.to_csv(sys.argv[2], index = False)
+    #points_df_5IK.to_csv(sys.argv[2], index = False)
     # For individual 5LK
     indiv = '5LK'
     data_indiv_5LK = prepare_dataset(data, season, indiv)
+    interpolate_coords(data_indiv_5LK, 45)
     interpolated_lat_17 = 17
-    interpolated_long_17 = 10.141
+    interpolated_long_17 = 10.137010 #10.141
     interpolated_lat_45 = 45
-    interpolated_long_45 = 9.69
-    data_indiv_5LK = interpolate_lat_and_long(data_indiv_5LK, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17)
+    interpolated_long_45 = 9.703307 #9.69
+    data_indiv_5LK = change_lat_and_long(data_indiv_5LK, interpolated_lat_45, interpolated_long_45, interpolated_lat_17, interpolated_long_17)
     path_5LK = from_df_to_list_of_tuples(data_indiv_5LK)
     points_5LK = place_equally_distanced_points(path_5LK, n_samples)
     points_df_5LK = plot_maps_migration(points_5LK, path_5LK)
-    points_df_5LK.to_csv(sys.argv[3], index = False)
+    #points_df_5LK.to_csv(sys.argv[3], index = False)
 
     # Calculate one-way distance for individual 5IK
     min_distance_list_5IK = select_shortest_distance(points_5IK, points_5LK)
     sum_distances_5IK = sum(min_distance_list_5IK)
-    tot_dist_migr = calc_dist_each_rand_loc(data, season)
-    sum_5IK = sum_distances_5IK/tot_dist_migr.loc[tot_dist_migr['ID'] == '5LK', 'distance'].values[0]
+    # Remember to use migration track cut off at 45 N and 17 N for tot_distance!
+    print('SUM 5LK first and then 5IK\n',data_indiv_5LK['distance'].sum(), data_indiv_5IK['distance'].sum())
+    # Recalculate all the distances of the segments now that the original dataframe is cut off at 17 and 45 lat N
+    data_indiv_5LK = add_distance_in_dataframe(data_indiv_5LK)
+    print('Total distance sum after recalculation',data_indiv_5LK['distance'].sum())
+    # Sum distances between track of individual A and track of individual B divided by total distance of track
+    # of individual A
+    sum_5IK = sum_distances_5IK / data_indiv_5IK['distance'].sum()
 
     # Calculate one-way distance for individual 5LK
     min_distance_list_5LK = select_shortest_distance(points_5LK, points_5IK)
     sum_distances_5LK = sum(min_distance_list_5LK)
-    sum_5LK = sum_distances_5LK/tot_dist_migr.loc[tot_dist_migr['ID'] == '5IK', 'distance'].values[0]
-    print(len(min_distance_list_5IK), len(min_distance_list_5LK))
+    # Recalculate all the distances of the segments now that the original dataframe is cut off at 17 and 45 lat N
+    data_indiv_5IK = add_distance_in_dataframe(data_indiv_5IK)
+    print('Total distance sum after recalculation',data_indiv_5IK['distance'].sum())
+    # Sum distances between track of individual B and track of individual A divided by total distance of track
+    # of individual B
+    sum_5LK = sum_distances_5LK / data_indiv_5LK['distance'].sum()
+
     # Final one-way distance
-    print('final one-way distance in km', sum_distances_5IK, sum_distances_5LK, sum_5IK, sum_5LK, (sum_5IK + sum_5LK)/2)
+    print('final one-way distance in km', (sum_5IK + sum_5LK)/2)
 
 
 
