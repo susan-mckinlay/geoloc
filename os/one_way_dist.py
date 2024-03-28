587ba4433ee1df6f188c36c80d82a6a736bb7fe8
Susan McKinlay <mc.ellen93@gmail.com>
2024-03-28 12:01:13 +0100

    I commented all the functions. Everything is in order

--- os/one_way_dist.py
+++ os/one_way_dist.py
@@ -77,7 +77,7 @@ def calc_dist_each_rand_loc(data, season):
 
 def prepare_dataset(data, season, indiv):
     """
-    Function to drop duplicates of 'modelat' and 'modelon' 
+    Function to drop duplicates of 'modelat' and 'modelon' column
     """
     data_indiv = data.loc[(data[f'compl_{season}_track'] == 1) & (data['season'] == season)\
          & (data['Juv'] == 1) & (data['ID'] == indiv)]
@@ -227,49 +227,49 @@ def plot_maps_migration(points, path):
     plt.plot(data['longitude'], data['latitude'],'bs') 
     plt.plot(points_df['longitude'], points_df['latitude'], 'rs') # Draw red squares
     mplleaflet.show()
-    
-    distance = 0
-    array_dist = []
-    i = 0
-    for _, row in points_df.iterrows():
-        if i == 0:
-            # first instance
-            prev_c = (row['latitude'], row['longitude'])
-            current_c = prev_c
-        else:
-            current_c = (row['latitude'], row['longitude'])
-        distance = calculate_distance(prev_c, current_c)
-        array_dist.append(distance)
-        prev_c = current_c
-        i += 1
-    points_df['distance'] = array_dist
-    
     return points_df
 
 def find_index_closest_value(data, limit):
+    """
+    Function that takes the data of the individual and the latitude limit (45 N or 17 N) and returns the
+    index of the row with the closest value to the limit
+    """
     # Find values that are closest to given values and return the row index
     difference = np.abs(limit - data['modelat'])
     index_closest_value = np.argmin(difference, axis=0)
     return index_closest_value
 
 def get_coordinates_between_interpolation(data_indiv, limit):
+    """
+    Function that takes the data of the individual and the latitude limit (45 N or 17 N) and returns the 
+    coordinates of point A and point B of the last segment of the migration track where I'll need to interpolate
+    the longitude of the point that has as latitude the limit (45 N or 17 N)
+    """
+    # Here I'm selecting all the points of the original migration track of the individual above the limit
     data_bigger = data_indiv.loc[(data_indiv['modelat'] > limit)]
     index_data_bigger = find_index_closest_value(data_bigger, limit)
+    # Get the progressive number of the coordinates of the point just above the limit
     prog_number_bigger = data_bigger['prog_number'].iloc[index_data_bigger]
-    print('progressive number above limit',data_bigger.loc[data_bigger['prog_number'] == prog_number_bigger, 'modelat'])
+    # With the selected progressive number use .loc to get the exact coordinates
     second_lat = data_bigger.loc[data_bigger['prog_number'] == prog_number_bigger, 'modelat'].values[0]
     second_long = data_bigger.loc[data_bigger['prog_number'] == prog_number_bigger, 'modelon'].values[0]
 
+    # Here I'm selecting all the points of the original migration track of the individual below the limit
     data_smaller = data_indiv.loc[(data_indiv['modelat'] < limit)]
     index_number_smaller = find_index_closest_value(data_smaller, limit)
+    # Get the progressive number of the coordinates of the point just below the limit
     prog_number_smaller = data_smaller['prog_number'].iloc[index_number_smaller]
-    print('progressive number below limit',data_smaller.loc[data_smaller['prog_number'] == prog_number_smaller, 'modelat'])
+    # With the selected progressive number use .loc to get the exact coordinates
     first_lat = data_smaller.loc[data_smaller['prog_number'] == prog_number_smaller, 'modelat'].values[0]
     first_long = data_smaller.loc[data_smaller['prog_number'] == prog_number_smaller, 'modelon'].values[0]
     
     return first_lat, second_lat, first_long, second_long
 
 def interpolate_coords(data_indiv, limit):
+    """
+    Function that takes the data of the individual and the latitude limit (45 N or 17 N) where I need to cut
+    off the final segment of the migration track of the individual. 
+    """
     # Get the coordinates to get the segment where I am going to find points every 100 metres
     first_lat, second_lat, first_long, second_long = get_coordinates_between_interpolation(data_indiv, limit)
     points = []
@@ -281,13 +281,15 @@ def interpolate_coords(data_indiv, limit):
     print('This is line.s13', line.s13)
     i = 0
     # s13 – distance of the segment in meters
+    # I iterate to get the coordinates of a point every 50 m until I get one that has the
     for point in range(tot_number_points + 1):
         if point == 0:
             print("point 0")
         distance_within_segment = min(distance * point, line.s13)
         interpolated_points = line.Position(distance_within_segment, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
         latitude = interpolated_points['lat2']
-        if (np.isclose(latitude, limit)) & (i == 0): # only take the first coordinate
+        if (np.isclose(latitude, limit)) & (i == 0): # only take the first coordinate with latitude similar
+            # to the limit (45N or 17N)
             print('Interpolated coordinates\n',(interpolated_points['lat2'], interpolated_points['lon2']))
             points.append((interpolated_points['lat2'], interpolated_points['lon2']))
             i += 1
@@ -300,12 +302,14 @@ def main():
     warnings.filterwarnings("ignore")
     data = pd.read_csv(sys.argv[1])
     data['date_time'] = pd.to_datetime(data['date_time'])
+
     # Season
     season = 'spr'
     # Only consider juveniles with complete tracks 
     data = data[(data[f'compl_{season}_track'] == 1) & (data['season'] == season) & (data['Juv'] == 1)]
     # Number of equally distanced points
     n_samples = 22
+
     # For individual 5IK
     indiv = '5IK'
     data_indiv_5IK = prepare_dataset(data, season, indiv)
@@ -317,13 +321,13 @@ def main():
     points_5IK = place_equally_distanced_points(path_5IK, n_samples)
     points_df_5IK = plot_maps_migration(points_5IK, path_5IK)
     points_df_5IK.to_csv(sys.argv[2], index = False)
+
     # For individual 5LK
     indiv = '5LK'
     data_indiv_5LK = prepare_dataset(data, season, indiv)
     # Interpolate coordinates at 45 and 17 of latitude (=cut-offs)
     points_45 = interpolate_coords(data_indiv_5LK, 45) # points_45 = (lat, lon)
     points_17 = interpolate_coords(data_indiv_5LK, 17) # points_17 = (lat, lon)
-    #print('This is interpolated lat 45', points_45[0], 'this is the longitude', points_45[1])
     data_indiv_5LK = change_lat_and_long(data_indiv_5LK, points_45[0], points_45[1], points_17[0], points_17[1])
     path_5LK = from_df_to_list_of_tuples(data_indiv_5LK)
     points_5LK = place_equally_distanced_points(path_5LK, n_samples)
